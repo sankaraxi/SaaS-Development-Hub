ad1f45636c06ad3cd0fd6f6c1050960808a631c0
sankaraxi <sankarfrompalani@gmail.com>
2024-11-16 18:57:57 +0530

    update home page view to render dynamic HTML content and add demo HTML file

--- src/escapadeSaaS/views.py
+++ src/escapadeSaaS/views.py
@@ -1,4 +1,33 @@
 from django.http import HttpResponse
+import pathlib
+
+this_file_path = pathlib.Path(__file__).resolve().parent # get the path of this file
+print(this_file_path) 
 
 def home_page_view(request):
-    return HttpResponse("<h1>Hello Django Saas</h1>")
+    # html_file = this_file_path / "demo.html"
+    # html_ = html_file.read_text()
+
+    title = "This Page And is the Home Page"
+
+    my_Content = {
+        "myPage": title,
+    }
+
+    html_ = """
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Escapade SaaS</title>
+        </head>
+        <body>
+            <h1>Escapade SaaS</h1>
+            <h3>Welcome to {myPage}</h3>
+        </body>
+        </html>
+    """.format(**my_Content)
+    
+    return HttpResponse(html_)  
